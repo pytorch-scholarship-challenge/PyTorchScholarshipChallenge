4a14a2b92631ccdf72a34c3e3726a5dd5ab29bc7
Marcin Sielski <marcin.sielski@gmail.com>
2018-11-10 21:32:05 +0100

    Update percepton.py

--- Lesson_2/10_Percepton_Algorithm/percepton.py
+++ Lesson_2/10_Percepton_Algorithm/percepton.py
@@ -44,8 +44,6 @@ def perceptronStep(X, y, W, b, learn_rate = 0.01):
 def trainPerceptronAlgorithm(X, y, learn_rate=0.01, num_epochs=25):
     x_min, x_max = min(X.T[0]), max(X.T[0])
     y_min, y_max = min(X.T[1]), max(X.T[1])
-    print(x_max)
-    print(X.T[0])
     W = np.array(np.random.rand(2, 1))
     b = np.random.rand(1)[0] + x_max
     # These are the solution lines that get plotted below.
